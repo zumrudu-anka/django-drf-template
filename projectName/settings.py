d2d70fe8e2b830ceddcd5a09995df9558a3d63ba
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2023-08-09 12:15:19 +0300

    Update settings.py

--- projectName/settings.py
+++ projectName/settings.py
@@ -94,5 +94,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Django DRF Ecommerce",
+    "TITLE": "Django DRF projectName",
 }
