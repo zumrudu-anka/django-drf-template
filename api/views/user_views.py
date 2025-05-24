198fa9884d0e10adf0acbbb7d0180208bd8f74eb
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2025-05-25 01:33:01 +0300

    Update user_views.py

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
-from api.serializers import UserSerializer, UserSerializerWithToken
+from api.serializers.user_serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
